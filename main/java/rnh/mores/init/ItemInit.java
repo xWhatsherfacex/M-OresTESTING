5ea78156ddb0416af9043bea1edb816cb5ed9ec4
xWhatsherfacex <epicfailteen@gmail.com>
2020-03-03 16:04:52 -0600

    Update ItemInit.java

--- main/java/rnh/mores/init/ItemInit.java
+++ main/java/rnh/mores/init/ItemInit.java
@@ -14,59 +14,11 @@ import rnh.mores.objects.items.RockItem;
 @ObjectHolder(Main.MODID)
 public class ItemInit {
 	
-	public static final Item dirt_item = null;
-	public static final Item gravel_item = null;
-	public static final Item sand_item = null;
-	public static final Item red_sand_item = null;
-	public static final Item soul_sand_item = null;
-
-	
-	public static final Item stone_rock_normal_item = null;
-	
-
-	public static final Item iron_ore_item = null;
-	public static final Item iron_poor_ore_item = null;
-	
-	public static final Item gold_ore_item = null;
-	public static final Item gold_poor_ore_item = null;
-	
-	public static final Item diamond_raw_item = null;
-	public static final Item diamond_raw_small_item = null;
-	
-	public static final Item emerald_raw_item = null;
-	public static final Item emerald_raw_small_item = null;
-	
-	
-	//Finitie
-	public static final Item finitie_ore = null;
-	public static final Item finitie_nugget = null;
-	public static final Item finitie_ingot = null;
-	
 	public static Item stone_sign_item;
 
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("dirt_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("coarse_dirt_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("gravel_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("sand_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("red_sand_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("soul_sand_item"));
 		
-		event.getRegistry().register(new RockItem(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("stone_rock_normal_item"));
-
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("iron_ore_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("iron_poor_ore_item"));
-		
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("gold_ore_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("gold_poor_ore_item"));
-
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("diamond_raw_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("diamond_raw_small_item"));
-		
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("emerald_raw_item"));
-		event.getRegistry().register(new Item(new Item.Properties().group(Main.MainItemGroup.instance)).setRegistryName("emerald_raw_small_item"));
-	
 		event.getRegistry().register(new BlockSignItem(new Item.Properties().group(Main.MainBlockGroup.instance), BlockInit.stone_standing_sign_block, BlockInit.stone_wall_sign_block).setRegistryName("stone_sign_item"));
 		
 	}
